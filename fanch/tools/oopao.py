1dd9dd03df45f45b4ceabd408eb40892ab708c24
fleroux <francois.leroux.pro@gmail.com>
2025-04-30 11:35:17 +0200

    closed loop delayed first try

--- fanch/tools/oopao.py
+++ fanch/tools/oopao.py
@@ -91,6 +91,86 @@ def close_the_loop(tel, ngs, atm, dm, wfs, reconstructor, loop_gain, n_iter,
     else:
         return total, residual, strehl
     
+
+def close_the_loop_delay(tel, ngs, atm, dm, wfs, reconstructor, loop_gain, n_iter, 
+                   delay=1, seed=0, save_telemetry=False, save_psf=False):
+    
+    ngs*tel
+    tel.computePSF()
+    
+    # Memory allocation
+    
+    total = np.zeros(n_iter)     # turbulence phase std [nm]
+    residual = np.zeros(n_iter)  # residual phase std [nm]
+    strehl = np.zeros(n_iter)    # Strehl Ratio
+    
+    if save_telemetry:
+        dm_coefs = np.zeros([dm.nValidAct, n_iter])
+        pupil_opd = np.zeros(list(tel.OPD.shape).append(n_iter))
+        wfs_frames = np.zeros(list(pyramid.cam.frame.shape).append(n_iter))
+        
+    if save_psf:
+        short_exposure_psf = np.zeros(list(tel.PSF.shape).append(n_iter))
+    
+    # initialization
+    
+    pupil_opd = np.zeros(list(tel.OPD.shape).append(delay))
+    atm.initializeAtmosphere(tel)
+    atm.generateNewPhaseScreen(seed = seed)
+    tel+atm
+    for n in range(1, delay):
+        pupil_opd[:,:,n] = tel.OPD
+        atm.update() # pupil_opd[:,:,delay] is the more recent turbulent phase screen
+
+    dm.coefs = 0
+    wfs_measure = 0*wfs.signal
+    ngs*tel*dm*wfs
+    
+    # close the loop
+    
+    for k in range(n_iter):
+        
+        # get total turbulence std
+        atm.update() # this should reset tel.OPD
+        total[k] = np.std(tel.OPD[np.where(tel.pupil==1)])*1e9 # after atm.update(), tel.OPD = turbulent phase screen.
+        
+        # save current turulent phase sreen to be corrected after the delay
+        np.roll(pupil_opd, -1, axis=0)
+        pupil_opd[:,:,-1] = tel.OPD
+        
+        tel*dm
+        
+        residual[k]=np.std(tel.OPD[np.where(tel.pupil==1)])*1e9 # [nm]
+        strehl[k] = np.exp(-np.var(tel.src.phase[np.where(tel.pupil==1)]))
+        
+        tel.OPD = pupil_OPD[0,:,:] # measuring the oldest OPD available
+        tel*dm*wfs
+        wfs_measure = wfs.signal
+        
+        # update dm coeficients
+        dm.coefs = dm.coefs - loop_gain * np.matmul(reconstructor, wfs_measure)
+                
+        if save_telemetry:
+            
+            dm_coefs[:,k] = dm.coefs
+            pupil_opd[:,:,k] = tel.OPD
+            wfs_frames[:,:,k] = pyramid.cam.frame
+            
+        if save_psf:
+            tel.computePSF()
+            short_exposure_psf[:,:,k] = tel.PSF
+    
+    # return
+    
+    if save_telemetry and save_psf:
+        return total, residual, strehl, dm_coefs, pupil_opd, wfs_frames, short_exposure_psf
+    elif save_telemetry:
+        return total, residual, strehl, dm_coefs, pupil_opd, wfs_frames
+    elif save_psf:
+        total, residual, strehl, short_exposure_psf
+    else:
+        return total, residual, strehl
+    
 def compute_mmse_reconstructor(calibratrion, modal_basis_covariance, noise_level, other_level):
     
     return 1
